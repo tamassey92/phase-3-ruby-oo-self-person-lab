0fd76ed271d4b1514a73f5d056a8a23cdb1bf88f
Taylor Massey <tamassey92@gmail.com>
2022-05-18 11:35:26 -0600

    commit tree 774222b48ceae0631a4e07f6243df8fe0c5f45bd

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,21 @@
 # your code goes here
+class Person
+
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+def bank_account=(value)
+    @bank_account = value
+end
+
+def bank_account
+    @bank_account
+end 
+
+end
